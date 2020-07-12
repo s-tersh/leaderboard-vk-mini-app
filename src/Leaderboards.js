f60544b8f2dfb7b53f90f4ddfe3967eda6f478c3
s-tersh <s.tersh@yandex.ru>
2020-07-13 01:43:44 +0300

    add refresh

--- src/Leaderboards.js
+++ src/Leaderboards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, View, Panel, PanelHeader, Tabs, TabsItem, Cell, HorizontalScroll } from '@vkontakte/vkui';
+import { Avatar, View, Panel, Tabs, TabsItem, Cell, HorizontalScroll, List, Group } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 export class Leaderboards extends React.Component {
@@ -8,21 +8,22 @@ export class Leaderboards extends React.Component {
         this.state = {
             activeModal: null,
             activePanel: "results",
-            viewGender: "M",
-            viewPage: "Result BSS 20",
-            data: this.props.data["Result BSS 20 M"].elements,
+            viewGender: this.props.gender,
+            viewPage: this.props.page,
+            data: this.props.data[this.props.page + " "  + this.props.gender].elements,
         };
 
         this.onChangeGender = this.onChangeGender.bind(this);
         this.onChangePage = this.onChangePage.bind(this);
         this.sumReps = this.sumReps.bind(this);
     }
-    
+
     onChangeGender(e) {
         this.setState({ 
             viewGender: e.currentTarget.dataset.gender,
             data: this.props.data[this.state.viewPage + " " + e.currentTarget.dataset.gender].elements,
         });
+        this.props.onChangePage(this.state.viewPage, e.currentTarget.dataset.gender);
     }
 
     onChangePage(e) {
@@ -30,6 +31,7 @@ export class Leaderboards extends React.Component {
             viewPage: e.currentTarget.dataset.page,
             data: this.props.data[e.currentTarget.dataset.page + " " + this.state.viewGender].elements
         });
+        this.props.onChangePage(e.currentTarget.dataset.page, this.state.viewGender);
     }
 
     sumReps(...theArgs) {
@@ -58,26 +60,29 @@ export class Leaderboards extends React.Component {
                             <TabsItem onClick={this.onChangePage} selected={viewPage === "BSS 20.3"} data-page="BSS 20.3">20.3</TabsItem>
                         </Tabs>
                     </HorizontalScroll>
-
-                    {data.map((item, id) => {
-                        let elem;
-                        switch(viewPage) {
-                            case "Result BSS 20":
-                                elem = <Cell key={id} data-index={item["#"]} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Сумма баллов"] + " points"}>{item["Участник"]}</Cell>
-                                break;
-                            case "BSS 20.1":
-                                elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={this.sumReps(parseInt(item["Повторения 1"]), parseInt(item["Повторения 2"])) === 0 ? "0 reps" : this.sumReps(parseInt(item["Повторения 1"]), parseInt(item["Повторения 2"])) + " reps"}>{item["Участник"]}</Cell>
-                                break;
-                            case "BSS 20.2":
-                                elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Суммарный вес"] + " kilos"}>{item["Участник"]}</Cell>
-                                break;
-                            case "BSS 20.3":
-                                elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Время"] + " | " + item["Повторения"] + " reps"}>{item["Участник"]}</Cell>
-                                break;
-                            default: break;
-                        }
-                        return elem;
-                    })}
+                    <Group>
+                        <List>
+                            {data.map((item, id) => {
+                                let elem;
+                                switch(viewPage) {
+                                    case "Result BSS 20":
+                                        elem = <Cell key={id} data-index={item["#"]} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Сумма баллов"] + " points"}>{item["Участник"]}</Cell>
+                                        break;
+                                    case "BSS 20.1":
+                                        elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={this.sumReps(parseInt(item["Повторения 1"]), parseInt(item["Повторения 2"])) === 0 ? "0 reps" : this.sumReps(parseInt(item["Повторения 1"]), parseInt(item["Повторения 2"])) + " reps"}>{item["Участник"]}</Cell>
+                                        break;
+                                    case "BSS 20.2":
+                                        elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Суммарный вес"] + " kilos"}>{item["Участник"]}</Cell>
+                                        break;
+                                    case "BSS 20.3":
+                                        elem = <Cell key={id} before={<Avatar><h3>{item["Место"]}</h3></Avatar>} description={item["Время"] + " | " + item["Повторения"] + " reps"}>{item["Участник"]}</Cell>
+                                        break;
+                                    default: break;
+                                }
+                                return elem;
+                            })}
+                        </List>
+                    </Group>
                 </Panel>
             </View>
         );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import { View, Panel, PanelHeader, PanelSpinner } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, PullToRefresh } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import bridge from '@vkontakte/vk-bridge';
 import Tabletop from 'tabletop';
@@ -14,16 +14,41 @@ export default class App extends React.Component {
     super();
     this.state = {
       data: {},
+      fetching: true,
+      viewGender: "M",
+      viewPage: "Result BSS 20"
     };
+    this.loadData = this.loadData.bind(this);
+    this.onRefresh = this.onRefresh.bind(this);
+  }
+
+  handlePage = (page, gender) => {
+    this.setState({
+      viewPage: page,
+      viewGender: gender
+    });
+  }
+
+  onRefresh(e) {
+    this.loadData();
+    
+    this.setState({
+      fetching: true
+    });
   }
 
   // get data from gsheets
   componentDidMount() {
+    this.loadData();
+  }
+  
+  loadData() {
     Tabletop.init({
     key: '1AqP7BUfuk1Yt_W6qByQDItnrzlHJHYP_3aJG3inKIhw',
     callback: googleData => {
         this.setState({
             data: googleData,
+            fetching: false
         }) 
     },
     simpleSheet: false
@@ -39,7 +64,9 @@ export default class App extends React.Component {
           <PanelHeader>
               <h3>Лидерборд</h3>
           </PanelHeader>
-          { Object.getOwnPropertyNames(data).length > 0 ? <Leaderboards data={data} /> : <PanelSpinner />}
+          <PullToRefresh onRefresh={this.onRefresh} isFetching={this.state.fetching}>
+            { Object.getOwnPropertyNames(data).length > 0 && this.state.fetching === false ? <Leaderboards data={data} page={this.state.viewPage} gender={this.state.viewGender} onChangePage={this.handlePage} /> : null}
+          </PullToRefresh>
         </Panel>
       </View>
     );
